a4c52e8efb2688641db11697b77928d28332c983
Yawson-Enoch <enochgybex25@gmail.com>
2023-04-14 22:22:03 +0000

    feat: add other response options

--- src/middleware/routeNotFound.middleware.ts
+++ src/middleware/routeNotFound.middleware.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import path from 'path';
 
 const routeNotFoundMiddleware = (req: Request, res: Response) => {
-  res.status(StatusCodes.NOT_FOUND).json({ msg: 'Route not found' });
+  res.status(StatusCodes.NOT_FOUND);
+
+  if (req.accepts('html')) {
+    return res.sendFile(path.resolve(__dirname, '..', 'public', '404.html'));
+  }
+
+  if (req.accepts('json')) {
+    return res.json({ msg: 'Route not found' });
+  }
+
+  return res.type('txt').send('Route not found');
 };
 
 export default routeNotFoundMiddleware;
