84cbadd2c3bfd29b11bfb77123c1e73481ed714c
Yawson-Enoch <enochgybex25@gmail.com>
2023-04-15 18:53:26 +0000

    feat: close server on uncaught exceptions and unhandled rejections

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,12 @@ import errorMiddleware from './middleware/error.middleware';
 import routeNotFoundMiddleware from './middleware/routeNotFound.middleware';
 import connectDb from './lib/connectDb';
 
+process.on('uncaughtException', (err) => {
+  console.error({ errorName: err.name, errorMessage: err.message });
+  console.error('UNCAUGHT EXCEPTION', '-', 'SHUTTING DOWN SERVER');
+  process.exit(1);
+});
+
 dotenv.config();
 
 const app = express();
@@ -21,13 +27,19 @@ app.use('/api/v1/auth', authRoute);
 app.use(routeNotFoundMiddleware);
 app.use(errorMiddleware);
 
-const port = process.env.PORT || 5000;
 connectDb(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(port, () =>
-      console.log(`Db connected and server is listening on port ${port}...`)
-    );
-  })
+  .then(() => console.log('Db connected successfully'))
   .catch((err: Error) =>
     console.error({ errorName: err.name, errorMessage: err.message })
   );
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () =>
+  console.log(`Server is listening on port ${port}...`)
+);
+
+process.on('unhandledRejection', (err: Error) => {
+  console.error({ errorName: err.name, errorMessage: err.message });
+  console.error('UNHANDLED REJECTION', '-', 'SHUTTING DOWN SERVER');
+  server.close(() => process.exit(1));
+});
