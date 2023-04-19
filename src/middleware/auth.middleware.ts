6004ee862ee8d813b548112aa34c9b2c426d2d48
Yawson-Enoch <enochgybex25@gmail.com>
2023-04-19 21:37:14 +0000

    refactor: change error text for failed auth check

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -15,7 +15,7 @@ const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith('Bearer')) {
-    throw new UnauthenticatedError('Not authorized to access this route');
+    throw new UnauthenticatedError('Authentication error');
   }
 
   const token = authHeader.split(' ')[1];
@@ -24,7 +24,7 @@ const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const decodedToken = jwt.verify(token, env.JWT_SECRET_KEY) as TypeTokenInfo;
     req.user = { userId: decodedToken.userId, username: decodedToken.username };
   } catch (error) {
-    throw new UnauthenticatedError('Not authorized to access this route');
+    throw new UnauthenticatedError('Authentication error');
   }
   next();
 };
